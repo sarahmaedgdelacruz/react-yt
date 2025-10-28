196de37c0241957395f7f179066062a7678b5508
Sarah <sarahmaedgdelacruz@gmail.com>
2025-10-28 12:02:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const darkMode = true;
+  return (
+    <>
+    <div className="bg-stone-700 p-2 flex h-lvh" id="main-container">
+      <div className="w-[50%] h-20 bg-pink-500"></div>
+      <div className="w-[50%] h-20 bg-purple-500"></div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/First.jsx
@@ -0,0 +1,34 @@
+function First() {
+    <>
+  return (
+    <div className='bg-blue-700 text-white shadow-xl shadow-cyan-500/50'>
+    App
+    </div>
+    <div className='bg-amber-500'>
+      <img className="aspect-3/2 object-center" src="https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" alt="" />
+
+      <iframe width="990" height="557" src="https://www.youtube.com/embed/WTktA94Ytt8?list=RDWTktA94Ytt8" title="Wale ft. Rihanna - Bad (Remix) Lyrics" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    </div>
+  )
+<div className='columns-3 gap-x-3 gap-y-4'>
+    <img src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2952&q=80" alt="" />
+    <img src="https://images.unsplash.com/photo-1463288889890-a56b2853c40f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3132&q=80" alt="" />
+    <img src="https://images.unsplash.com/photo-1526400473556-aac12354f3db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" alt="" />
+    <img src="https://images.unsplash.com/photo-1434394354979-a235cd36269d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2902&q=80" alt="" />
+    <img src="https://images.unsplash.com/photo-1611605645802-c21be743c321?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" alt="" />
+    <img src="https://images.unsplash.com/photo-1617369120004-4fc70312c5e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80" alt="" />
+    <img src="https://images.unsplash.com/photo-1491904768633-2b7e3e7fede5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3131&q=80" alt="" />
+    <img src="https://images.unsplash.com/photo-1498603993951-8a027a8a8f84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2936&q=80" alt="" />
+    <img src="https://images.unsplash.com/photo-1518892096458-a169843d7f7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" alt="" />
+    <div className='bg-linear-to-r from-indigo-500 to bg-pink-500'>
+      App
+    </div>
+    </div>
+    <div className='flex'>
+    <div className={darkMode ? "bg-black text-white" : "hidden"}>This is a content</div>
+    <button className='bg-pink-600 text-white px-3 py-1 rounded-sm m-3 cursor-pointer hover:bg-pink-900 inline-'>Dark Mode</button>
+    </div>
+    </>
+}
+
+export default First
